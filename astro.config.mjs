48de115fa753c2ef660ac030a775863c180f9605
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2026-01-19 13:53:17 +0530

    Added CloudFlare Pages and Wrangler settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  output:'server',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+
+  integrations: [react()],
+  adapter: cloudflare({
+    platformProxy : {
+      enabled:true
+    }
+  })
 });
