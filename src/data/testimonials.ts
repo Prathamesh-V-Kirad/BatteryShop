f5aabf2b746fdc697996cc2c671f4cac06d3a053
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2026-04-06 20:15:57 +0530

    Added Testimonials

---
+++ src/data/testimonials.ts
@@ -0,0 +1,28 @@
+export type Testimonial = {
+  name: string;
+  role: string;
+  text: string;
+};
+export const testimonials: Testimonial[] = [
+{
+    name: "Shishir Suhane",
+    role: "Local Guide",
+    text: "What an amazing service by the owner himself, I am just WoW! I am staying in Koparkhairane. I wanted to change the inverter and its battery. The owner has really guided me with the best selection of the battery and the inverter for my home.I am really satisfied with their service. It was an overwhelming service I have received, hence writing the review.",
+  },
+  {
+    name: "Mudit Awasti",
+    role: "Customer",
+    text: "Best in Navi Mumbai, I recommend evryone to go at National Battery only.",
+  },
+  {
+    name: "Shahzad Malik",
+    role: "Customer",
+    text: "Excellent service 👍.",
+  },
+  
+  {
+    name: "Anup Sharma",
+    role: "Customer",
+    text: "All battery service",
+  }
+]
